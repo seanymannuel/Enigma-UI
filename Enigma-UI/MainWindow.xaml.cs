77ac2932d69150e1add587690328aa8dcdf3d1dd
Sean <symannuel21@gmail.com>
2023-09-27 00:22:38 +0800

    ONTI NALANG

--- Enigma-UI/MainWindow.xaml.cs
+++ Enigma-UI/MainWindow.xaml.cs
@@ -32,37 +32,141 @@ namespace Enigma_UI
 
         }
 
+        private bool shiftKeyPressed = false;
+        private bool uppercaseEnabled = false;
+        private bool capsLockEnabled = false;
+        private bool capsLockKeyPressed = false;
+
+
         private void InitializeEventHandlers()
-        {
-            // Attach key down and key up event handlers to the window.
+        {            
             KeyDown += Window_KeyDown;
             KeyUp += Window_KeyUp;
         }
 
+
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
+            // Handle the Shift key
+            if (e.Key == Key.LeftShift || e.Key == Key.RightShift)
+            {
+                shiftKeyPressed = true;
+                uppercaseEnabled = true; // Enable uppercase mode when Shift is pressed
+                shift.Background = Brushes.Yellow;
+                return;
+            }
+
+            if (e.Key == Key.CapsLock)
+            {
+                capsLockKeyPressed = true;
+                capsLockEnabled = !capsLockEnabled;
+                // Update your Shift Lock label text or appearance here
+                shiftlock.Content = capsLockEnabled ? "Caps Lock" : "Shift Lock";
+                shiftlock.Background = Brushes.Yellow;
+                return;
+            }
+
+            if (uppercaseEnabled)
+            {
+                char character = KeyManager(e.Key);
+                character = char.ToUpper(character);
+                // Append the uppercase character to your output or handle it as needed.
+            }
+
+            else 
+            { 
             // Change the background color of the Label when a key is pressed.
-          if (e.Key == Key.A) 
-            {
-                a.Background = Brushes.Yellow;
-            }
+            switch (e.Key)
+                {
+                    case Key.A:
+                        a.Background = Brushes.Yellow;
+                        break;
+                    case Key.B:
+                        b.Background = Brushes.Yellow;
+                        break;
+                    case Key.C:
+                        cc.Background = Brushes.Yellow;
+                        break;
+                    case Key.D:
+                        d.Background = Brushes.Yellow;
+                        break;
+                    case Key.E:
+                        ee.Background = Brushes.Yellow;
+                        break;
+                    case Key.F:
+                        f.Background = Brushes.Yellow;
+                        break;
+                    case Key.G:
+                        g.Background = Brushes.Yellow;
+                        break;
+                    case Key.H:
+                        h.Background = Brushes.Yellow;
+                        break;
+                    case Key.I:
+                        i.Background = Brushes.Yellow;
+                        break;
+                    case Key.J:
+                        j.Background = Brushes.Yellow;
+                        break;
+                    case Key.K:
+                        k.Background = Brushes.Yellow;
+                        break;
+                    case Key.L:
+                        el.Background = Brushes.Yellow;
+                        break;
+                    case Key.M:
+                        m.Background = Brushes.Yellow;
+                        break;
+                    case Key.N:
+                        n.Background = Brushes.Yellow;
+                        break;
+                    case Key.O:
+                        o.Background = Brushes.Yellow;
+                        break;
+                    case Key.P:
+                        p.Background = Brushes.Yellow;
+                        break;
+                    case Key.Q:
+                        q.Background = Brushes.Yellow;
+                        break;
+                    case Key.R:
+                        r.Background = Brushes.Yellow;
+                        break;
+                    case Key.S:
+                        s.Background = Brushes.Yellow;
+                        break;
+                    case Key.T:
+                        t.Background = Brushes.Yellow;
+                        break;
+                    case Key.U:
+                        u.Background = Brushes.Yellow;
+                        break;
+                    case Key.V:
+                        v.Background = Brushes.Yellow;
+                        break;
+                    case Key.W:
+                        w.Background = Brushes.Yellow;
+                        break;
+                    case Key.X:
+                        x.Background = Brushes.Yellow;
+                        break;
+                    case Key.Y:
+                        y.Background = Brushes.Yellow;
+                        break;
+                    case Key.Z:
+                        z.Background = Brushes.Yellow;
+                        break;
+                        // OTHER KEYPAD
+                    case Key.Space:
+                        spacebar.Background = Brushes.Yellow;
+                        break;
 
-            else if (e.Key == Key.B)
-            {
-                b.Background = Brushes.Yellow;
-            }
-            
-            else if (e.Key == Key.C)
-            {
-                c.Background = Brushes.Yellow;
-            }
-            
-            else if (e.Key == Key.C)
-            {
-                c.Background = Brushes.Yellow;
-            }
 
-
+                    case Key.Back:
+                        _return.Background = Brushes.Yellow;
+                        break;
+                }
+            }
         }
 
         private char KeyManager(Key input)
@@ -71,75 +175,140 @@ namespace Enigma_UI
 
             if (input == Key.Space)
                 retVal = ' ';
+            else if (capsLockEnabled || (shiftKeyPressed && !capsLockKeyPressed)) // Check if Caps Lock is enabled or Shift is pressed (but not Caps Lock)
+                retVal = char.ToUpper(input.ToString()[0]); // Convert to uppercase
             else
-                retVal = input.ToString()[0];
+                retVal = char.ToLower(input.ToString()[0]); // Convert to lowercase
 
             return retVal;
         }
 
+
+
+
         private void Window_KeyUp(object sender, KeyEventArgs e)
         {
-
-            if (initialState)
+            if (e.Key == Key.LeftShift || e.Key == Key.RightShift)
             {
-                initialState = false;
-                output.Content = KeyManager(e.Key);
+                shiftKeyPressed = false;
+                uppercaseEnabled = false; // Disable uppercase mode when Shift is released
+                shift.Background = Brushes.Transparent;
+                return;
+            }
+
+            if (e.Key == Key.CapsLock)
+            {
+                capsLockKeyPressed = false;
+                shiftlock.Background = Brushes.Transparent;
+                return;
+            }
+
+            if (e.Key == Key.Back)
+            {
+                if (!string.IsNullOrEmpty(output.Content.ToString()))
+                {
+                    output.Content = output.Content.ToString().Substring(0, output.Content.ToString().Length - 1);
+                    _return.Background = Brushes.Transparent;
+                }
             }
             else
-                output.Content += KeyManager(e.Key) + "";
+            {
+                if (initialState)
+                {
+                    initialState = false;
+                    output.Content = KeyManager(e.Key);
+                }
+                else
+                    output.Content += KeyManager(e.Key) + "";
+            }
+
             // Revert the background color when the key is released.
-            /*
-            if (e.Key == Key.A)
-            {
-                a.Background = Brushes.Transparent;
+            switch (e.Key)
+            {              
+                case Key.A:
+                    a.Background = Brushes.Transparent;
+                    break;
+                case Key.B:
+                    b.Background = Brushes.Transparent;
+                    break;
+                case Key.C:
+                    cc.Background = Brushes.Transparent;
+                    break;
+                case Key.D:
+                    d.Background = Brushes.Transparent;
+                    break;
+                case Key.E:
+                    ee.Background = Brushes.Transparent;
+                    break;
+                case Key.F:
+                    f.Background = Brushes.Transparent;
+                    break;
+                case Key.G:
+                    g.Background = Brushes.Transparent;
+                    break;
+                case Key.H:
+                    h.Background = Brushes.Transparent;
+                    break;
+                case Key.I:
+                    i.Background = Brushes.Transparent;
+                    break;
+                case Key.J:
+                    j.Background = Brushes.Transparent;
+                    break;
+                case Key.K:
+                    k.Background = Brushes.Transparent;
+                    break;
+                case Key.L:
+                    el.Background = Brushes.Transparent;
+                    break;
+                case Key.M:
+                    m.Background = Brushes.Transparent;
+                    break;
+                case Key.N:
+                    n.Background = Brushes.Transparent;
+                    break;
+                case Key.O:
+                    o.Background = Brushes.Transparent;
+                    break;
+                case Key.P:
+                    p.Background = Brushes.Transparent;
+                    break;
+                case Key.Q:
+                    q.Background = Brushes.Transparent;
+                    break;
+                case Key.R:
+                    r.Background = Brushes.Transparent;
+                    break;
+                case Key.S:
+                    s.Background = Brushes.Transparent;
+                    break;
+                case Key.T:
+                    t.Background = Brushes.Transparent;
+                    break;
+                case Key.U:
+                    u.Background = Brushes.Transparent;
+                    break;
+                case Key.V:
+                    v.Background = Brushes.Transparent;
+                    break;
+                case Key.W:
+                    w.Background = Brushes.Transparent;
+                    break;
+                case Key.X:
+                    x.Background = Brushes.Transparent;
+                    break;
+                case Key.Y:
+                    y.Background = Brushes.Transparent;
+                    break;
+                case Key.Z:
+                    z.Background = Brushes.Transparent;
+                    break;
+                    // OTHER KEYPAD
+                case Key.Space:
+                    spacebar.Background = Brushes.Transparent;
+                    break;
             }
-
-            else if (e.Key == Key.B)
-            {
-                b.Background = Brushes.Transparent;
-            }
-
-            else if (e.Key == Key.C)
-            {
-                c.Background = Brushes.Transparent;
-            }
-            */
         }
     }
 }
-
-
-//using System;
-//using System.Windows;
-//using System.Windows.Controls;
-//using System.Windows.Input;
-//using System.Windows.Media;
-
-//namespace WpfKeyHighlight
-//{
-//    public partial class MainWindow : Window
-//    {
-//        public MainWindow()
-//        {
-//            InitializeComponent();
-//            InitializeEventHandlers();
-//        }
-
-//        private void InitializeEventHandlers()
-//        {
-//            // Attach the PreviewKeyDown event handler to the window.
-//            PreviewKeyDown += Window_PreviewKeyDown;
-//        }
-
-//        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
-//        {
-//            // Check if the pressed key is a letter or a number.
-//            if (Char.IsLetterOrDigit((char)e.Key))
-//            {
-//                // Change the background color of the Label when a letter or number key is pressed.
-//                highlightLabel.Background = Brushes.Yellow; // You can set any color you like.
-//            }
-//        }
-//    }
-//}
 
